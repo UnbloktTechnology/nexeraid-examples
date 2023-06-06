f52b7bcb7370c9caa9e83e5f2bb9d1ee47a172d7
facu <facundogregori@hotmail.com>
2023-06-06 18:02:11 -0300

    add prettier

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,17 +1,12 @@
 const getAccessToken = async (address: string) => {
-  const response = await fetch(
-    `/api/access_token?address=${address}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "GET",
-    }
-  );
+  const response = await fetch(`/api/access_token?address=${address}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "GET",
+  });
   const { accessToken } = await response.json();
-  return accessToken
-}
+  return accessToken;
+};
 
-export {
-  getAccessToken
-}
+export { getAccessToken };
--- src/utils/getConfig.ts
+++ src/utils/getConfig.ts
@@ -1,11 +1,9 @@
-import config from "../config/config.json"
+import config from "../config/config.json";
 
 const getConfig = () => {
-    const env = process.env.NEXT_PUBLIC_ENVIROMENT ?? 'production'
-    // @ts-ignore
-    return config[env.toLowerCase()]
-}
+  const env = process.env.NEXT_PUBLIC_ENVIROMENT ?? "production";
+  // @ts-ignore
+  return config[env.toLowerCase()];
+};
 
-export {
-    getConfig
-}
+export { getConfig };

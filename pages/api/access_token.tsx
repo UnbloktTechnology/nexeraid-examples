d0e424de990ce0d503ce8d2424e02267e08602cc
Lizandro Zerpa <lizandroal.zerpa@gmail.com>
2023-06-08 15:08:07 -0400

    Fix API Key update

--- pages/api/access_token.tsx
+++ pages/api/access_token.tsx
@@ -6,23 +6,20 @@ import { getConfig } from "../../src/utils/getConfig";
  * This has to be done from secured server, to avoid leaking API_KEY
  */
 const getAccessToken = async (req: NextApiRequest, res: NextApiResponse) => {
-  const config = getConfig()
+  const config = getConfig();
   const API_KEY = config.apiKey;
   const query = req.query;
   const { address } = query;
-  const apiHost = config.api
+  const apiHost = config.api;
   try {
-    const response = await fetch(
-      `${apiHost}kyc/auth/access-token`,
-      {
-        body: JSON.stringify({ publicAddress: address }),
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${API_KEY}`,
-        },
-        method: "POST",
-      }
-    );
+    const response = await fetch(`${apiHost}kyc/auth/access-token`, {
+      body: JSON.stringify({ publicAddress: address }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+      method: "POST",
+    });
     const { accessToken, ...error } = await response.json();
     console.log("ACEESS TOKEN ", accessToken, error);
     res.status(200).json({ accessToken });

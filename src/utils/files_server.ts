3ee04c4a9cfcdb174b97fb65052213572231b912
Ayrton Paredes <ayrton@thinkanddev.com>
2023-06-09 23:19:42 -0400

    Fixing styles and folder write

--- src/utils/files_server.ts
+++ src/utils/files_server.ts
@@ -1,13 +1,22 @@
-import { writeFileSync, readFileSync, unlinkSync } from 'fs';
+import { writeFileSync, readFileSync, unlinkSync, existsSync, mkdirSync } from 'fs';
+
+const FOLDER_PATH = './my-data-webhook';
 
 export const writeFile = (name: string, data: string) => {
-  writeFileSync(`/tmp/${name}.txt`, data);    
+  createFolderIfNotExists();
+  writeFileSync(`${FOLDER_PATH}/${name.toLowerCase()}.txt`, data);
 }
 
 export const readFile = (name: string) => {
-  return readFileSync(`/tmp/${name}.txt`)
+  return readFileSync(`${FOLDER_PATH}/${name.toLowerCase()}.txt`)
 }
 
 export const deleteFile = (name: string) => {
-  return unlinkSync(`/tmp/${name}.txt`)
+  return unlinkSync(`${FOLDER_PATH}/${name.toLowerCase()}.txt`)
+}
+
+const createFolderIfNotExists = () => {
+  if (!existsSync(FOLDER_PATH)) {
+    mkdirSync(FOLDER_PATH, { recursive: true });
+  }
 }

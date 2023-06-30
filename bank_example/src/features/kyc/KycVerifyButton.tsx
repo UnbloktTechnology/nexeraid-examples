7a1fc37ace95a2307d732f5caed3b205c3d6bd8f
grmkris <kristjan-ext@allianceblock.io>
2023-06-30 09:32:50 +0200

    fix example-kyc

--- bank_example/src/features/kyc/KycVerifyButton.tsx
+++ bank_example/src/features/kyc/KycVerifyButton.tsx
@@ -1,3 +1,54 @@
+import { useGlobalModals } from "@/features/Modals/useGlobalModals";
+import { useKycAuthentication } from "@/features/kyc/useKycAuthenticate";
+import { KYC_CLIENTS } from "@/features/kyc/KycClient";
+import { useEffect } from "react";
+import { getSigner } from "@/appConfig";
+import { toast } from "react-toastify";
+import { useCheckCompliance } from "@/features/kyc/useCheckCompliance";
+
 export const KycVerifyButton = () => {
+  const { close } = useGlobalModals((state) => ({
+    close: state.close,
+  }));
+  const { accessToken, signingMessage, signature, user } =
+    useKycAuthentication();
+  const checkCompliance = useCheckCompliance();
+  const kycClient = KYC_CLIENTS.verify;
+
+  useEffect(() => {
+    if (user && accessToken && signingMessage && signature && kycClient) {
+      console.log("init kyc client", {
+        accessToken,
+        signingMessage,
+        signature,
+      });
+      kycClient.onSignPersonalData(async (data: string) => {
+        console.log("on sign personal data");
+        const signer = getSigner(user);
+        return await signer.signMessage(data);
+      });
+      kycClient.onOffChainShareCompletition(() => {
+        void (async () => {
+          const result = await checkCompliance.mutateAsync();
+          console.log("result", result);
+          if (result) {
+            toast(`Your identity has been verified`);
+            close();
+          } else {
+            toast(`Your identity has not been verified`);
+          }
+        })();
+      });
+      kycClient.init({
+        auth: {
+          accessToken,
+          signingMessage,
+          signature,
+        },
+        initOnFlow: "REQUEST",
+      });
+    }
+  }, [user, accessToken, signingMessage, signature]);
+
   return <button id="kyc-btn-verify">Verify</button>;
 };
--- bank_example/src/pages/index.tsx
+++ bank_example/src/pages/index.tsx
@@ -1,56 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { DisclaimerOverlay } from "@/features/Components/DisclaimerOverlay";
 import { Dashboard } from "@/features/Dashboard";
 
 import { Banner, Content, Header, Layout } from "@/features/Layout";
 import { useGlobalModals } from "@/features/Modals/useGlobalModals";
-import { toast } from "react-toastify";
-import { useKycAuthentication } from "@/features/kyc/useKycAuthenticate";
 import { useCheckCompliance } from "@/features/kyc/useCheckCompliance";
-import { KYC_CLIENTS } from "@/features/kyc/KycClient";
-import { getSigner } from "@/appConfig";
 
 const Home = () => {
-  const { openModal, close } = useGlobalModals((state) => ({
+  const { openModal } = useGlobalModals((state) => ({
     openModal: state.open,
     close: state.close,
   }));
-  const { accessToken, signingMessage, signature, user } =
-    useKycAuthentication();
   const checkCompliance = useCheckCompliance();
-  const kycClient = KYC_CLIENTS.verify;
-
   console.log("checkCompliance", checkCompliance.data);
-
-  useEffect(() => {
-    if (user && accessToken && signingMessage && signature && kycClient) {
-      kycClient.onSignPersonalData(async (data: string) => {
-        console.log("on sign personal data");
-        const signer = getSigner(user);
-        return await signer.signMessage(data);
-      });
-      kycClient.onOffChainShareCompletition(() => {
-        void (async () => {
-          const result = await checkCompliance.mutateAsync();
-          console.log("result", result);
-          if (result) {
-            toast(`Your identity has been verified`);
-            close();
-          } else {
-            toast(`Your identity has not been verified`);
-          }
-        })();
-      });
-      kycClient.init({
-        auth: {
-          accessToken,
-          signingMessage,
-          signature,
-        },
-        initOnFlow: "REQUEST",
-      });
-    }
-  }, [user, accessToken, signingMessage, signature, kycClient]);
 
   const onClickLogOn = () => {
     openModal(

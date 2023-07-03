186d20631ddd850645d32ac55f064c5d39feff68
grmkris <kristjan-ext@allianceblock.io>
2023-07-03 18:18:45 +0200

    more stuff

--- defi_example/src/features/kyc/useKycAuthenticate.tsx
+++ defi_example/src/features/kyc/useKycAuthenticate.tsx
@@ -1,52 +1,43 @@
-import { useEffect } from "react";
 import KycClient from "@nexeraid/kyc-sdk/client";
 import { useMutation } from "@tanstack/react-query";
-import { decodeJwt } from "jose";
-import { useAccount, useSignMessage } from "wagmi";
 import { create } from "zustand";
 import { createJSONStorage, devtools, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import { api } from "@/utils/api";
-
-import { AddressSchema } from "@nexeraprotocol/nexera-id-schemas";
+import { useSignMessage } from "wagmi";
+import { Address } from "viem";
 
 export const useKycAuthentication = () => {
-  const { address } = useAccount();
-  const { signMessageAsync } = useSignMessage();
   const authStore = useAuthStore((state) => state);
   const getAccessToken = api.access.accessToken.useMutation();
+  const { signMessageAsync } = useSignMessage();
 
   const logout = useMutation(async () => {
     await Promise.resolve(authStore.logout());
   });
 
   const authenticate = useMutation(
-    async () => {
-      if (!address) {
-        throw new Error("Missing data to authenticate");
-      }
-      const signingMessage = KycClient.buildSignatureMessage(
-        address.toLowerCase()
-      );
+    async (variables: { user: Address }) => {
+      const signingMessage = KycClient.buildSignatureMessage(variables.user);
       const signature = await signMessageAsync({ message: signingMessage });
       const response = await getAccessToken.mutateAsync({
-        address: address.toLowerCase(),
+        address: variables.user,
       });
       const { accessToken } = response;
       return {
         accessToken,
         signingMessage,
         signature,
+        testUser: variables.user,
       };
     },
     {
       onSuccess: (data) => {
-        const validatedAddress = AddressSchema.parse(address);
         authStore.authenticate(
-          validatedAddress,
           data.accessToken,
           data.signingMessage,
-          data.signature
+          data.signature,
+          data.testUser
         );
       },
       onError: (error) => {
@@ -55,46 +46,28 @@ export const useKycAuthentication = () => {
     }
   );
 
-  useEffect(() => {
-    if (authStore.address && address) {
-      if (authStore.address.toLowerCase() !== address.toLowerCase()) {
-        logout.mutate();
-      }
-      if (authStore.accessToken) {
-        const token = decodeJwt(authStore?.accessToken);
-        if (!token.exp) throw new Error("Invalid token, missing token.EXP");
-        const jwtDate = new Date(token.exp * 1000);
-        // if jwt has more than 1 hour to live, use it
-        const now = new Date();
-
-        now.setHours(now.getHours() - 1);
-        if (jwtDate < now) {
-          logout.mutate();
-        }
-      }
-    }
-  }, [address, authStore.accessToken, authStore.address, logout]);
-
   return {
     authenticate,
     logout,
     accessToken: authStore.accessToken,
     signingMessage: authStore.signingMessage,
     signature: authStore.signature,
+    isAuthenticated: authStore.isAuthenticated,
+    user: authStore.user,
   };
 };
 
 interface IAuthStore {
-  address?: string;
   accessToken?: string;
   signingMessage?: string;
   signature?: string;
   isAuthenticated: boolean;
+  user?: Address;
   authenticate: (
-    address: string,
     accessToken: string,
     signingMessage: string,
-    signature: string
+    signature: string,
+    user: Address
   ) => void;
   logout: () => void;
 }
@@ -104,31 +77,30 @@ const useAuthStore = create<IAuthStore>()(
     persist(
       immer((set) => ({
         accessToken: undefined,
-        address: undefined,
         signature: undefined,
         signingMessage: undefined,
         isAuthenticated: false,
-        authenticate: (address, accessToken, signingMessage, signature) => {
+        authenticate: (accessToken, signingMessage, signature, user) => {
           set((state) => {
-            state.address = address;
             state.accessToken = accessToken;
             state.signingMessage = signingMessage;
             state.signature = signature;
             state.isAuthenticated = true;
+            state.user = user;
           });
         },
         logout: () => {
           set((state) => {
-            state.address = undefined;
             state.accessToken = undefined;
             state.signingMessage = undefined;
             state.signature = undefined;
             state.isAuthenticated = false;
+            state.user = undefined;
           });
         },
       })),
       {
-        name: "kyc-demo-auth-store",
+        name: "bank-demo-auth-store",
         storage: createJSONStorage(() => sessionStorage),
       }
     )

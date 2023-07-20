49ac2f0d6e78ff101c15ff0578f2de424cddd37a
Lizandro Zerpa <lizandroal.zerpa@gmail.com>
2023-07-20 15:46:54 -0400

    Fixed undefined redis data

--- defi_example/src/server/api/routers/complianceRouter.ts
+++ defi_example/src/server/api/routers/complianceRouter.ts
@@ -26,39 +26,44 @@ export const complianceRouter = createTRPCRouter({
     .mutation(async ({ input }) => {
       const redisKey = getDataWebhookRedisKey(input.address);
       const redisData = await redis.get(redisKey);
-      const body = {
-        address: input.address,
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
-        inputData: { credentials: redisData?.data?.credentials || '' },
-        scenarioId: env.NEXERA_SCENARIO_ID,
-      };
-      console.log(`Got ${redisKey} from redis`, {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
-        body: body.inputData.credentials?.[0].type,
-      });
-      const result = await fetch(
-        `${
-          appConfig[env.NEXT_PUBLIC_ENVIRONMENT].api
-        }compliance/scenario/execute`,
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${env.NEXERA_ID_API_KEY}`,
-          },
-          body: JSON.stringify(body),
-        }
-      );
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const json = await result.json();
-      console.log(`Got result from Nexera`, {
+
+      if (redisData) {
+        const body = {
+          address: input.address,
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+          inputData: { credentials: redisData?.data?.credentials || '' },
+          scenarioId: env.NEXERA_SCENARIO_ID,
+        };
+        console.log(`Got ${redisKey} from redis`, {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+          body: body.inputData.credentials?.[0].type,
+        });
+        const result = await fetch(
+          `${
+            appConfig[env.NEXT_PUBLIC_ENVIRONMENT].api
+          }compliance/scenario/execute`,
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${env.NEXERA_ID_API_KEY}`,
+            },
+            body: JSON.stringify(body),
+          }
+        );
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        json,
-      });
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return json;
+        const json = await result.json();
+        console.log(`Got result from Nexera`, {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          json,
+        });
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return json;
+      }
+      
+      return undefined
     }),
 });

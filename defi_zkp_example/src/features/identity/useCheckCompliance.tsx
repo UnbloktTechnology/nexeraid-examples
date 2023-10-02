dd881878309e5146ddf757034018134e8c9592ca
fgregoribattista <35629983+fgregoribattista@users.noreply.github.com>
2023-10-02 15:15:12 -0300

    Fix defi zkp (#150)
    * add lock file
    
    * refact defi zkp offchain

--- defi_zkp_example/src/features/identity/useCheckCompliance.tsx
+++ defi_zkp_example/src/features/identity/useCheckCompliance.tsx
@@ -6,7 +6,7 @@ export const useCheckCompliance = (enabled: boolean) => {
   const { user } = useKycAuthentication();
   const mutation = api.compliance.executeRule.useMutation();
 
-  const checkCompliance = useQuery({
+  return useQuery({
     queryKey: ["checkCompliance", enabled],
     queryFn: async () => {
       if (!user) return Promise.resolve(false);
@@ -16,14 +16,7 @@ export const useCheckCompliance = (enabled: boolean) => {
       });
       console.log("isCompliant result", result);
       return result
-        ? result.every((compliant) => {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-            return compliant.result.result.validate?.[0].is_valid as boolean;
-          })
-        : false;
     },
     enabled,
   });
-
-  return { checkCompliance };
 };

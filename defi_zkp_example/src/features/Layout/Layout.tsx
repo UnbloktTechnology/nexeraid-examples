1dcb74f69df137a9447054bfa31522de0866d0ef
Marko <1616694+markoftw@users.noreply.github.com>
2023-10-02 11:46:25 +0200

    Update example defi (#147)
    * fix readme
    
    * add missing files
    
    * fix lint
    
    * add branch changes
    
    * wip

--- defi_zkp_example/src/features/Layout/Layout.tsx
+++ defi_zkp_example/src/features/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { type ReactNode } from "react";
 import { GlobalModals } from "@/features/Modals/GlobalModals";
 
 export type BackgroundType = "default" | "defi";

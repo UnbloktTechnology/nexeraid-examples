fbc6121dd5260116b477bc91d0db78ed7a488f4f
Marko <1616694+markoftw@users.noreply.github.com>
2023-10-02 11:05:40 +0200

    move to a separate project instead of branch (#144)

---
+++ defi_zkp_example/src/appConfig.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const ENVS = ["local", "dev", "stage", "prod"] as const;
+export const ENVSchema = z.enum(ENVS);
+
+export const appConfig = {
+  [ENVSchema.enum.local]: {
+    api: "http://localhost:3001/",
+  },
+  [ENVSchema.enum.dev]: {
+    api: "https://api-dev.nexera.id/",
+  },
+  [ENVSchema.enum.stage]: {
+    api: "https://api-staging.nexera.id/",
+  },
+  [ENVSchema.enum.prod]: {
+    api: "https://api.nexera.id/",
+  },
+} as const;
---
+++ defi_zkp_example/src/features/identity/LogOnModal.tsx
@@ -0,0 +1,38 @@
+import { IdentityVerifyButton } from "@/features/identity/IdentityVerifyButton";
+import { useKycAuthentication } from "@/features/identity/useKycAuthenticate";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
+import { toast } from "react-toastify";
+
+export const LogOnModal = () => {
+  const account = useAccount();
+  const { authenticate, isAuthenticated } = useKycAuthentication();
+
+  return (
+    <div className="flex w-full flex-col items-center gap-10">
+      <div className="flex w-full flex-col gap-4">
+        <h3 className="text-2xl text-white">Verify your identity to be able to swap</h3>
+      </div>
+
+      <div className="flex w-full flex-col items-center">
+        <ConnectButton />
+      </div>
+
+      {account.address && !isAuthenticated && (
+        <button
+          className={`ml-auto rounded-md bg-[blue] px-6 py-4 text-base font-bold text-white`}
+          onClick={() => {
+            if (!account.address) {
+              toast("Please connect your wallet first");
+              return;
+            }
+            authenticate.mutate({ user: account.address });
+          }}
+        >
+          Log in
+        </button>
+      )}
+      {account.address && isAuthenticated && <IdentityVerifyButton />}
+    </div>
+  );
+};

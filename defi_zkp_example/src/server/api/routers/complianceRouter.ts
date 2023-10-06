6000e3f1e59b4d756eb6fc99641e2212d4d48c81
fgregoribattista <35629983+fgregoribattista@users.noreply.github.com>
2023-10-06 13:30:41 -0300

    refact validation offchain zkrpoof (#167)
    * refact validation offchain zkrpoof
    
    * fix lint

--- defi_zkp_example/src/server/api/routers/complianceRouter.ts
+++ defi_zkp_example/src/server/api/routers/complianceRouter.ts
@@ -3,39 +3,41 @@ import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { redis } from "@/server/redis";
 import { getScenarioWebhookRedisKey } from "@/pages/api/scenario-webhook";
 
-export const DATA_STATUS = z.enum(['received', 'not_received'])
+export const DATA_STATUS = z.enum(["received", "not_received"]);
+interface keyable {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+}
 
 export const complianceRouter = createTRPCRouter({
   executeRule: publicProcedure
     .input(
       z.object({
         address: z.string(),
-      })
+      }),
     )
     .output(
       z.object({
         data: DATA_STATUS,
-        isValid: z.boolean()
-      })
+        isValid: z.boolean(),
+      }),
     )
     .mutation(async ({ input }) => {
-
-      console.log('getScenarioWebhookRedisKey')
       const redisKey = getScenarioWebhookRedisKey(input.address);
-      const redisData = await redis.get(redisKey);
+      const redisData = await redis.get<keyable>(redisKey);
 
-      console.log("REDIS DATA: ", redisData)
-      if (redisData) {
+      console.log("REDIS DATA: ", redisData);
+      Object.keys({ asd: "asd" }).length;
+
+      if (Object.keys(redisData?.result as object).length > 0) {
         return {
-          data: 'received',
-          isValid: true
-        }
+          data: "received",
+          isValid: true,
+        };
       }
-
       return {
-        data: 'not_received',
-        isValid: false
-      }
+        data: "not_received",
+        isValid: false,
+      };
     }),
 });
-

91e6433786db24e1f4afb0f76e28c0413edce315
Lizandro Zerpa <lizandroal.zerpa@gmail.com>
2024-02-09 11:43:18 +0100

    NEX-784 - Checking extra things related with KTC widget

--- example-apps/src/pages/api/sygnum-web3/scenario-webhook.ts
+++ example-apps/src/pages/api/sygnum-web3/scenario-webhook.ts
@@ -12,14 +12,16 @@ export const getScenarioWebhookBankWeb3RedisKey = (address: string) => {
 
 const scenarioWebHookPost = async (
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) => {
   if (req.method === "POST") {
     console.log("=== scenarioWebHookPost SIGNUM req.body===", req.body);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const body: ScenarioWebhookPayload = req.body;
-    const key = getScenarioWebhookBankWeb3RedisKey(body.address);
-    await redis.set(key, JSON.stringify(body));
+    if (!((body.result as unknown) instanceof Array)) {
+      const key = getScenarioWebhookBankWeb3RedisKey(body.address);
+      await redis.set(key, JSON.stringify(body));
+    }
     res.status(200).json({ response: "ok" });
   }
   if (req.method === "GET") {

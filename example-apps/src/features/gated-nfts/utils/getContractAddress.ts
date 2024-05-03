dee912a9464f311a2a2d625bae50f481d96500ea
grmkris <kristjan.grm1@gmail.com>
2024-05-03 19:50:34 +0200

    update-gated-example-with-latest-sdk

--- example-apps/src/features/gated-nfts/utils/getContractAddress.ts
+++ example-apps/src/features/gated-nfts/utils/getContractAddress.ts
@@ -2,25 +2,29 @@ import type { ChainId } from "@nexeraprotocol/identity-schemas";
 import { NEXERA_CHAINS } from "@nexeraprotocol/identity-schemas";
 import {
   ExampleGatedNFTMinterAddress_amoy_dev,
-  ExampleGatedNFTMinterAddress_mumbai_dev,
   ExampleGatedNFTMinterAddress_sepolia_dev,
   ExampleNFTMinterAddress_amoy_dev,
-  ExampleNFTMinterAddress_mumbai_dev,
   ExampleNFTMinterAddress_sepolia_dev,
 } from "@nexeraprotocol/nexera-id-sig-gating-contracts-sdk/addresses";
 
 export const getGatedContractAddress = (chainId: ChainId) => {
-  return chainId == NEXERA_CHAINS.SEPOLIA
-    ? ExampleGatedNFTMinterAddress_sepolia_dev
-    : chainId == NEXERA_CHAINS.POLYGON_AMOY
-      ? ExampleGatedNFTMinterAddress_amoy_dev
-      : ExampleGatedNFTMinterAddress_mumbai_dev;
+  switch (chainId) {
+    case NEXERA_CHAINS.SEPOLIA:
+      return ExampleGatedNFTMinterAddress_sepolia_dev;
+    case NEXERA_CHAINS.POLYGON_AMOY:
+      return ExampleGatedNFTMinterAddress_amoy_dev;
+    default:
+      throw new Error("Chain not supported");
+  }
 };
 
 export const getNonGatedContractAddress = (chainId: ChainId) => {
-  return chainId == NEXERA_CHAINS.SEPOLIA
-    ? ExampleNFTMinterAddress_sepolia_dev
-    : chainId == NEXERA_CHAINS.POLYGON_AMOY
-      ? ExampleNFTMinterAddress_amoy_dev
-      : ExampleNFTMinterAddress_mumbai_dev;
+  switch (chainId) {
+    case NEXERA_CHAINS.SEPOLIA:
+      return ExampleNFTMinterAddress_sepolia_dev;
+    case NEXERA_CHAINS.POLYGON_AMOY:
+      return ExampleNFTMinterAddress_amoy_dev;
+    default:
+      throw new Error("Chain not supported");
+  }
 };
--- example-apps/src/features/gated-nfts/utils/useMintNFT.tsx
+++ example-apps/src/features/gated-nfts/utils/useMintNFT.tsx
@@ -1,13 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
-import type {
-  Account,
-  Chain,
-  Client,
-  PublicActions,
-  RpcSchema,
-  Transport,
-  WalletActions,
-} from "viem";
+import { encodeFunctionData } from "viem";
 
 import { ExampleGatedNFTMinterABI } from "@nexeraprotocol/nexera-id-sig-gating-contracts-sdk/abis";
 
@@ -20,26 +12,21 @@ import {
   useChainId,
   useAccount,
   useBlockNumber,
-  useWriteContract,
+  usePublicClient,
+  useWalletClient,
 } from "wagmi";
 import { getGatedContractAddress } from "./getContractAddress";
+import { waitForTransactionReceipt } from "viem/actions";
 
 const WRONG_SIGNATURE: EIP155Signature =
   "0xc6fd40ac16944fd0fef20071149270a2c283c8ae92ffcbb5e61f44348490dc3b65e786637aaa82f46ac3c01941a9875046a2ceb9bad189362014b35f6e74df231b";
 
-export type WalletClientExtended = Client<
-  Transport,
-  Chain,
-  Account,
-  RpcSchema,
-  PublicActions & WalletActions<Chain, Account>
->;
 export const useMintGatedNFTFromSDK = () => {
   const chainId = useChainId();
   const account = useAccount();
   const blockNumber = useBlockNumber();
-
-  const mintNFTGatedFromSDK = useWriteContract();
+  const signer = useWalletClient();
+  const publicClient = usePublicClient();
 
   return useMutation({
     mutationFn: async () => {
@@ -47,41 +34,61 @@ export const useMintGatedNFTFromSDK = () => {
         console.log("IDENTITY_CLIENT is not initizalied");
         return { signatureResponse: { isAuthorized: false } };
       }
+      if (!account.address) {
+        throw new Error("No account in wallet Client - address");
+      }
+
+      if (!signer.data) {
+        throw new Error("No signer in wallet Client - data");
+      }
+
+      if (!publicClient) {
+        throw new Error("No publicClient in wallet Client");
+      }
       try {
         if (!account.address) {
           throw new Error("No account in wallet Client - address");
         }
 
-        const txAuthInput = {
+        const signatureResponse = await IDENTITY_CLIENT.getTxAuthSignature({
           contractAbi: Array.from(ExampleGatedNFTMinterABI),
           contractAddress: getGatedContractAddress(ChainId.parse(chainId)),
           functionName: "mintNFTGated",
           args: [account.address],
           chainId: ChainId.parse(chainId),
-        };
-
-        const signatureResponse =
-          await IDENTITY_CLIENT.getTxAuthSignature(txAuthInput);
+        });
 
         // If user is not authorized, use wrong signature and dummy blockExpiratioin
         const blockExpiration =
           signatureResponse.blockExpiration ??
           (blockNumber.data ? Number(blockNumber.data) + 10 : 0);
-        const signature = signatureResponse.signature ?? WRONG_SIGNATURE;
+        const signatureData =
+          signatureResponse.signatureData ?? WRONG_SIGNATURE;
 
         // Mint Gated Nft with signature
-        const result = await mintNFTGatedFromSDK.writeContractAsync({
-          address: getGatedContractAddress(ChainId.parse(chainId)),
+        const unsignedTx = encodeFunctionData({
           abi: ExampleGatedNFTMinterABI,
           functionName: "mintNFTGated",
-          args: [account.address, BigInt(blockExpiration), signature],
+          args: [account.address],
+        });
+
+        // Complete data
+        const txData = (unsignedTx + signatureData) as `0x${string}`;
+
+        const tx = await signer.data.sendTransaction({
+          to: getGatedContractAddress(ChainId.parse(chainId)),
+          data: txData,
+        });
+
+        const receipt = await waitForTransactionReceipt(publicClient, {
+          hash: tx,
         });
 
         return {
-          txHash: result,
+          txHash: receipt.transactionHash,
           signatureResponse: {
             isAuthorized: signatureResponse.isAuthorized,
-            signature,
+            signatureData: signatureData,
             blockExpiration,
           },
         };

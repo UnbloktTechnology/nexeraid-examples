68f5f51b50752287f01c61b11179f929fb33fddd
Kevin Pannetier <pannetier.kevin@gmail.com>
2024-07-11 23:00:02 +0200

    Feature/web3 441 create an mvp example UI (#285)
    * feat(peaq): add basic landing page
    
    * feat(peaq): flow WIP
    
    * feat(peaq): flow WIP
    
    * feat(peaq): flow done, v1

--- example-apps/src/features/kyc-airdrop/components/KYCAirdrop.tsx
+++ example-apps/src/features/kyc-airdrop/components/KYCAirdrop.tsx
@@ -1,86 +1,57 @@
 import { useState } from "react";
-import { useWalletClient, useWaitForTransactionReceipt } from "wagmi";
-
+import { useWalletClient } from "wagmi";
 import type { MintResponse } from "../utils/blockchain.schema";
-import { useGetTokenBalance } from "../utils/useGetTokenBalance";
 import { useClaimToken } from "../utils/useClaimToken";
-
-import { DisplayApiResponse } from "./DisplayApiResponse";
-import { DisplayTokenBalance } from "./DisplayTokenBalance";
-
-const buttonStyle = {
-  padding: "16px 24px",
-  borderRadius: 16,
-  cursor: "pointer",
-  backgroundColor: "#0258FD",
-  color: "white",
-  fontWeight: "bold",
-  fontSize: "16px",
-  border: "none",
-};
+import { useKYCContext } from "../providers/KYCContext";
+import { Button } from "./Button";
 
 export const KYCAirdrop = (props: { did: string | undefined }) => {
   const { did } = props;
   const { data: walletClient } = useWalletClient();
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [sdkResponse, setSdkResponse] = useState<MintResponse | undefined>(
     undefined,
   );
-
-  const getBalance = useGetTokenBalance();
-
   const tryClaiming = useClaimToken();
-  const claimTxResult = useWaitForTransactionReceipt({
-    hash: tryClaiming.data?.txHash ?? "0x0",
-    query: { enabled: !!tryClaiming.data?.txHash },
-  });
+  const {
+    isWalletWhitelisted,
+    isWalletChecked,
+    setIsWalletFailedClaim,
+    setIsWalletClaimed,
+  } = useKYCContext();
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <>
-      <div>DID:{did}</div>
-      {!did && <div>Waiting for Polygon Wallet instantiation...</div>}
-      {did && (
-        <>
-          <div className="m-4 w-full border border-black p-4">
-            <h1 className={"text-3xl font-bold"}>KYC Gated Airdrop</h1>
-            <br />
-            <button
-              style={buttonStyle}
-              id="mint-sdk-btn"
-              disabled={!walletClient}
-              onClick={() => {
-                if (walletClient) {
-                  tryClaiming
-                    .mutateAsync()
-                    .then((_sdkResponse) => {
-                      setSdkResponse(_sdkResponse);
-                    })
-                    .catch((e) => {
-                      console.log("error while fetching signature", e);
-                    });
-                } else {
-                  console.log("walletClient not loaded");
-                }
-              }}
-            >
-              Claim Token
-            </button>
-            <br />
-            <h2 className={"mt-4 text-2xl font-bold"}>SDK RESPONSE</h2>
-            <DisplayApiResponse
-              mintResponse={sdkResponse}
-              gasCost={claimTxResult.data?.gasUsed}
-              writeData={{
-                isLoading: tryClaiming.isPending,
-                isSuccess: tryClaiming.isSuccess,
-              }}
-              error={tryClaiming.data?.error}
-            />
-            <br />
-            <DisplayTokenBalance balance={getBalance.balance ?? 0} />
-          </div>
-          <br />
-        </>
+      {did && isWalletChecked && isWalletWhitelisted && (
+        <Button
+          className="rounded-full bg-white px-4 py-2 font-medium text-black shadow hover:bg-gray-100"
+          id="mint-sdk-btn"
+          disabled={!walletClient}
+          isLoading={isLoading}
+          onClick={() => {
+            if (walletClient) {
+              setIsLoading(true);
+              tryClaiming
+                .mutateAsync()
+                .then((_sdkResponse) => {
+                  setIsLoading(false);
+                  setSdkResponse(_sdkResponse);
+                  setIsWalletClaimed(true);
+                })
+                .catch((e) => {
+                  setIsLoading(false);
+                  console.log("error while fetching signature", e);
+                  setIsWalletFailedClaim(true);
+                });
+            } else {
+              console.log("walletClient not loaded");
+            }
+          }}
+        >
+          2 - Claim to connect wallet address
+        </Button>
       )}
     </>
   );
--- example-apps/src/features/kyc-widget/KYCClientEIP155.tsx
+++ example-apps/src/features/kyc-widget/KYCClientEIP155.tsx
@@ -1,15 +1,26 @@
 import { useAccount, useSignMessage } from "wagmi";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { IdentityFlow } from "./IdentityFlow";
+import { IdentityFlowAirdrop } from "./IdentityFlowAirdrop";
+import { useEffect } from "react";
+import { useKYCContext } from "../kyc-airdrop/providers/KYCContext";
 
 export const KYCClientEIP155 = (props: { setDID: (did: string) => void }) => {
   const signMessage = useSignMessage();
   const { address } = useAccount();
+
+  const { isWalletChecked, resetKYCContext } = useKYCContext();
+
+  useEffect(() => {
+    if (!address && isWalletChecked) {
+      resetKYCContext();
+    }
+  }, [address, isWalletChecked, resetKYCContext]);
+
   return (
     <>
-      <ConnectButton />
+      <ConnectButton label="Connect the wallet" />
       {address && (
-        <IdentityFlow
+        <IdentityFlowAirdrop
           setDID={props.setDID}
           signMessageAsync={async (message: string) => {
             return await signMessage.signMessageAsync({ message });

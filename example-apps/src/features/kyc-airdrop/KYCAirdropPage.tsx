1f6928746ee65fa7438acf3df19619b54b8762cb
Antoine Estienne <estienne.antoine@gmail.com>
2024-07-12 11:08:37 +0200

    use balance instead of isWalletCLaimed

--- example-apps/src/features/kyc-airdrop/KYCAirdropPage.tsx
+++ example-apps/src/features/kyc-airdrop/KYCAirdropPage.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import stylesPeaq from "./peaq.module.css";
 import KYCAirdropFooter from "./components/ui/KYCAirdropFooter";
 import { useKYCContext } from "./providers/KYCContext";
+import { useGetTokenBalance } from "./utils/useGetTokenBalance";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,9 +24,15 @@ export const KYCAirdropPage = () => {
   const [subtitle, setSubtitle] = useState<string>(
     "Connect your wallet to claim tokens",
   );
+  const balance = useGetTokenBalance();
 
   useEffect(() => {
-    if (isWalletChecked && isWalletWhitelisted) {
+    if (balance.balance && Number(balance.balance) > 0) {
+      setTitle("Tokens claimed successfully");
+      setSubtitle(
+        `Congrats! Your wallet should have claimed your ${balance.balance} tokens`,
+      );
+    } else if (isWalletChecked && isWalletWhitelisted) {
       setTitle("This wallet qualifies");
       setSubtitle(
         "Congrats! Now we need to verify your identity before you can claim your tokens",
@@ -35,9 +42,6 @@ export const KYCAirdropPage = () => {
       setSubtitle(
         "Unfortunately, this wallet doesn't qualify for the airdrop. Please try again with a different wallet",
       );
-    } else if (isWalletClaimed) {
-      setTitle("Tokens claimed successfully");
-      setSubtitle("Congrats! Your wallet should have claimed the tokens");
     } else if (isWalletFailedClaim) {
       setTitle("Tokens claim unsuccessful");
       setSubtitle("Unfortunately, we can't allow token claim for this wallet");
@@ -47,6 +51,7 @@ export const KYCAirdropPage = () => {
     isWalletWhitelisted,
     isWalletChecked,
     isWalletFailedClaim,
+    balance,
   ]);
 
   return (
@@ -74,10 +79,12 @@ export const KYCAirdropPage = () => {
             <section className="w-full max-w-xl rounded-lg px-8 py-20 text-center text-white">
               <h1 className="font-aeonix-regular mb-4 text-4xl">{title}</h1>
               <h2 className="font-aeonix-regular mb-4 text-2xl">{subtitle}</h2>
-              <div className="mb-4 flex flex-col items-center justify-center gap-4">
-                <KYCClientAirdrop setDID={setDID} />
-                <KYCAirdrop did={did} />
-              </div>
+              {(balance.balance == undefined || balance.balance === 0) && (
+                <div className="mb-4 flex flex-col items-center justify-center gap-4">
+                  <KYCClientAirdrop setDID={setDID} />
+                  <KYCAirdrop did={did} />
+                </div>
+              )}
               <p className="mt-4">Terms and conditions</p>
             </section>
           </div>

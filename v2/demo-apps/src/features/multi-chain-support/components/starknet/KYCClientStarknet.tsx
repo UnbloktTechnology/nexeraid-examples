afbef532f304cbcc2d0bc89813e9d95f7b3b1dad
Clément PREVOST <998369+prevostc@users.noreply.github.com>
2024-08-20 11:29:36 +0200

    Add v2 examples

---
+++ v2/demo-apps/src/features/multi-chain-support/components/starknet/KYCClientStarknet.tsx
@@ -0,0 +1,34 @@
+import { IdentityFlow } from "../../../kyc-widget/IdentityFlow";
+import ConnectStarknet from "./ConnectStarknet";
+import type { Account } from "starknet";
+
+import {
+	signWithStarknet,
+	useStarknetWallet,
+} from "../../utils/useStarknetWallet";
+
+export const KYCClientStarknet = (props: { setDID: (did: string) => void }) => {
+	const { wallet } = useStarknetWallet();
+	return (
+		<>
+			<div className="pt-10 text-center">
+				<p className="text-2xl font-bold">
+					NexeraID Kyc Demo: Starknet Wallet KYC
+				</p>
+			</div>
+			<div className="min-w-prose mt-24 flex flex-col items-center justify-center pt-2">
+				<ConnectStarknet />
+				{wallet && (
+					<IdentityFlow
+						setDID={props.setDID}
+						signMessageAsync={async (message: string) => {
+							return await signWithStarknet(message, wallet.account as Account);
+						}}
+						address={wallet.selectedAddress}
+						blockchainNamespace={"starknet"}
+					/>
+				)}
+			</div>
+		</>
+	);
+};

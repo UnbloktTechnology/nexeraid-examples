67a15fa6dc045030b2bd77e8c471153aa229e46f
Clément PREVOST <998369+prevostc@users.noreply.github.com>
2024-08-29 11:48:35 +0200

    Fix review comments

--- v2/airdrop-platform/app/src/configureDemoEnv.tsx
+++ v2/airdrop-platform/app/src/configureDemoEnv.tsx
@@ -2,6 +2,8 @@ import { env } from "@/env.mjs";
 import { _setInternalConfig as _setInternalConfigReact } from "@nexeraid/react-sdk";
 import { _setInternalConfig as _setInternalConfigNode } from "@nexeraid/js-sdk";
 
+// this is for internal nexeraid use only
+
 console.log("_setInternalConfigReact", { env: env.NEXT_PUBLIC_ENVIRONMENT });
 _setInternalConfigReact({ env: env.NEXT_PUBLIC_ENVIRONMENT });
 console.log("_setInternalConfigNode", { env: env.NEXT_PUBLIC_ENVIRONMENT });

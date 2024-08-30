0c5e71552b62b29b79f2713a33dcae6e9b29491c
Clément <998369+prevostc@users.noreply.github.com>
2024-08-30 14:32:07 +0200

    Add contract addresses (#360)

--- v2/airdrop-platform/app/src/kyc-airdrop/config/EXAMPLE_AIRDROP_CONTRACT_ADDRESSES.ts
+++ v2/airdrop-platform/app/src/kyc-airdrop/config/EXAMPLE_AIRDROP_CONTRACT_ADDRESSES.ts
@@ -62,9 +62,54 @@ const EXAMPLE_AIRDROP_CONTRACT_ADDRESSES: {
       distributor: "0xc15BAd8De67c6b2294C8ec27694Fd12B734cE4C5",
     },
   },
-  cicd: {},
-  "test-dev-1": {},
-  "test-dev-2": {},
-  dev: {},
-  stage: {},
+  cicd: {
+    [NEXERA_EVM_CHAINS.SEPOLIA]: {
+      token: "0x48182d21869b874BabdeCC0851dDA4F89B18a687",
+      distributor: "0x96caF7D5DD0304976A070804074E453887BE509c",
+    },
+    [NEXERA_EVM_CHAINS.POLYGON_AMOY]: {
+      token: "0x83236b1a94AEC9f1Fd3Ad35ddf838139EA4b9729",
+      distributor: "0xc15BAd8De67c6b2294C8ec27694Fd12B734cE4C5",
+    },
+  },
+  "test-dev-1": {
+    [NEXERA_EVM_CHAINS.SEPOLIA]: {
+      token: "0x48182d21869b874BabdeCC0851dDA4F89B18a687",
+      distributor: "0x96caF7D5DD0304976A070804074E453887BE509c",
+    },
+    [NEXERA_EVM_CHAINS.POLYGON_AMOY]: {
+      token: "0x83236b1a94AEC9f1Fd3Ad35ddf838139EA4b9729",
+      distributor: "0xc15BAd8De67c6b2294C8ec27694Fd12B734cE4C5",
+    },
+  },
+  "test-dev-2": {
+    [NEXERA_EVM_CHAINS.SEPOLIA]: {
+      token: "0x48182d21869b874BabdeCC0851dDA4F89B18a687",
+      distributor: "0x96caF7D5DD0304976A070804074E453887BE509c",
+    },
+    [NEXERA_EVM_CHAINS.POLYGON_AMOY]: {
+      token: "0x83236b1a94AEC9f1Fd3Ad35ddf838139EA4b9729",
+      distributor: "0xc15BAd8De67c6b2294C8ec27694Fd12B734cE4C5",
+    },
+  },
+  dev: {
+    [NEXERA_EVM_CHAINS.SEPOLIA]: {
+      token: "0x48182d21869b874BabdeCC0851dDA4F89B18a687",
+      distributor: "0x96caF7D5DD0304976A070804074E453887BE509c",
+    },
+    [NEXERA_EVM_CHAINS.POLYGON_AMOY]: {
+      token: "0x83236b1a94AEC9f1Fd3Ad35ddf838139EA4b9729",
+      distributor: "0xc15BAd8De67c6b2294C8ec27694Fd12B734cE4C5",
+    },
+  },
+  stage: {
+    [NEXERA_EVM_CHAINS.SEPOLIA]: {
+      token: "0x48182d21869b874BabdeCC0851dDA4F89B18a687",
+      distributor: "0x96caF7D5DD0304976A070804074E453887BE509c",
+    },
+    [NEXERA_EVM_CHAINS.POLYGON_AMOY]: {
+      token: "0x83236b1a94AEC9f1Fd3Ad35ddf838139EA4b9729",
+      distributor: "0xc15BAd8De67c6b2294C8ec27694Fd12B734cE4C5",
+    },
+  },
 } as const;

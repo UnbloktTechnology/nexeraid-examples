711f997f73f3bad6c39fc1528e55d455fa49ca89
CamilleNexera <camille@nexera.id>
2024-09-10 16:27:26 +0200

    Update nexera-config.ts

--- v2/frontend-noframework-web3/nexera-config.ts
+++ v2/frontend-noframework-web3/nexera-config.ts
@@ -1,7 +1,7 @@
 //This config file is used to generate a challenge by using the API created in the backend.
 
 export const generateChallenge = async (params: any) => {
-    const res = await fetch("http://localhost:5000/api/generate-web3-challenge", {
+    const res = await fetch("yourApiUrl/api/generate-web3-challenge", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",

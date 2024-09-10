d77f8c60a92fcd3cac154074d79e795233bebcb9
CamilleNexera <camille@nexera.id>
2024-09-10 11:55:26 +0200

    NextJS Rainbowkit Web3 example (#366)
    Co-authored-by: Cam <camille@device.local>

---
+++ v2/nextjs-web3/src/nexera-config.ts
@@ -0,0 +1,13 @@
+//This config file is used to generate a challenge by using the API created in the backend.
+
+export const generateChallenge = async (params: any) => {
+    const res = await fetch("/api/challenge", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+    });
+
+    return res.json();
+};

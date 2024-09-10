0b9d04b483af618c7137f6b0f2f5132d8bf0e150
Cam <camille@device.local>
2024-09-10 11:37:49 +0200

    NextJS Rainbowkit Web3 example

---
+++ v2/nextjs-web3/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ["@nexeraid/react-sdk-wallet-wagmi"],
+};
+
+module.exports = nextConfig;
---
+++ v2/nextjs-web3/src/pages/_app.tsx
@@ -0,0 +1,43 @@
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { config } from '../wagmi';
+import { NexeraIdProvider } from "@nexeraid/react-sdk";
+import { createWagmiWalletAdapter } from "@nexeraid/react-sdk-wallet-wagmi";
+import { createWeb3AuthAdapter, createConfig } from "@nexeraid/react-sdk";
+import { generateChallenge } from "../nexera-config";
+
+// this is for internal nexeraid use only
+import { _setInternalConfig as _setInternalConfigReact } from "@nexeraid/react-sdk";
+import { _setInternalConfig as _setInternalConfigNode } from "@nexeraid/js-sdk";
+
+console.log("_setInternalConfigReact", { env: 'dev' });
+_setInternalConfigReact({ env: 'dev' });
+console.log("_setInternalConfigNode", { env: 'dev' });
+_setInternalConfigNode({ env: 'dev' });
+
+
+const walletAdapter = createWagmiWalletAdapter(config);
+const authAdapter = createWeb3AuthAdapter({ generateChallenge, wallet: walletAdapter });
+const nexeraConfig = createConfig({ authAdapter });
+
+const client = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={client}>
+        <RainbowKitProvider>
+          <NexeraIdProvider config={nexeraConfig}>
+            <Component {...pageProps} />
+          </NexeraIdProvider>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default MyApp;

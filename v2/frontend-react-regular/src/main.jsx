63f1c46d01c5395949ba9e3f4e8622136c31266d
CamilleNexera <camille@nexera.id>
2024-09-10 16:30:01 +0200

    Update main.jsx

--- v2/frontend-react-regular/src/main.jsx
+++ v2/frontend-react-regular/src/main.jsx
@@ -5,17 +5,6 @@ import './index.css'
 import { NexeraIdProvider } from "@nexeraid/react-sdk";
 import { createAuthAdapter, createConfig } from "@nexeraid/react-sdk";
 import { createSession } from "./nexera-config.ts";
-
-
-// this is for internal nexeraid use only
-import { _setInternalConfig as _setInternalConfigReact } from "@nexeraid/react-sdk";
-import { _setInternalConfig as _setInternalConfigNode } from "@nexeraid/js-sdk";
-
-console.log("_setInternalConfigReact", { env: 'dev' });
-_setInternalConfigReact({ env: 'dev' });
-console.log("_setInternalConfigNode", { env: 'dev' });
-_setInternalConfigNode({ env: 'dev' });
-
 const authAdapter = createAuthAdapter({ createSession });
 const nexeraConfig = createConfig({ authAdapter });
 
